72cacb83bde24eaa134e1f45b32c7eac9439c912
Krisztian <webevolution13@gmail.com>
2020-05-20 15:18:48 +0200

    add static freh version

--- js/karaKrisz.js
+++ js/karaKrisz.js
@@ -1,21 +1,17 @@
 $(document).ready(function () {
     // executes when HTML-Document is loaded and DOM is ready
-    console.log("document is ready");
-
-
+    //console.log("document is ready");
     $('[data-toggle="offcanvas"], #navToggle').on('click', function () {
-        $('.offcanvas-collapse').toggleClass('open')
+        $('.offcanvas-collapse').toggleClass('open');
+        $('.orientation').animate({
+            width: "toggle"
+        }, 300, 'linear');
     })
-
-
     // document ready  
 });
 
-
-window.onload = function () {
-    // executes when complete page is fully loaded, including all frames, objects and images
-    console.log("window is loaded");
-
-
-    // window load  
-};
+// window.onload = function () {
+//     // executes when complete page is fully loaded, including all frames, objects and images
+//     console.log("window is loaded");
+//     // window load  
+// };
